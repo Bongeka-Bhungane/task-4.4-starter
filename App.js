d057bd6c9793defacc557221dfd6465b673ce017
Bongeka-Bhungane <angelabhungane@gmsil.com>
2025-08-06 13:35:19 +0200

    added some functionality to the add task button

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 const mainElement = document.getElementById("root");
 document.body.appendChild(mainElement);
 
-const taskContainer = document.createElement("div");
-
-taskContainer.style.backgroundColor = "lightblue";
-    taskContainer.style.padding = "10px";
-    taskContainer.style.margin = "10px 0";
-
 const taskInput = document.createElement("input"); //add an input
-root.appendChild(taskInput);
 taskInput.type = "text";
 taskInput.placeholder = "Enter a new task...";
+taskInput.style.padding = "10px"; // style for the div
+taskInput.style.margin = "10px 0";
+root.appendChild(taskInput);
 
 const addButton = document.createElement("button"); //button for adding
 addButton.textContent = "Add Task";
+addButton.style.backgroundColor = "black"
+addButton.style.color = "white"
+root.appendChild(addButton);
 
-mainElement.style.backgroundColor = 'lightblue';
-root.appendChild(taskContainer);
+addButton.addEventListener("click", function () {
+    const newTask = document.createElement("div");
+      newTask.textContent = "New Task";
+      newTask.style.padding = "10px";
+      newTask.style.marginTop = "5px";
+      newTask.style.backgroundColor = "lightgray";
 
+      root.appendChild(newTask);
+});
